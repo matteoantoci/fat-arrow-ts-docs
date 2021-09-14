cd2f8cd98cddf560735646a03ec5da8c8d71bd28
Matteo Antoci <matteo.antoci@lastminute.com>
2021-09-14 11:35:00 +0200

    First logo version

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { FC } from 'react'
 import clsx from 'clsx'
 import Layout from '@theme/Layout'
 import Link from '@docusaurus/Link'
@@ -6,12 +6,12 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import styles from './index.module.css'
 import HomepageFeatures from '../components/HomepageFeatures'
 
-function HomepageHeader() {
+const HomepageHeader: FC = () => {
   const { siteConfig } = useDocusaurusContext()
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className='container'>
-        <h1 className='hero__title'>{siteConfig.title}</h1>
+        <h1 className='hero__title'>Fat Arrow</h1>
         <p className='hero__subtitle'>{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
@@ -25,16 +25,13 @@ function HomepageHeader() {
   )
 }
 
-export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext()
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description='Description will go into a meta tag in <head />'>
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  )
-}
+export default (): JSX.Element => (
+  <Layout
+    title={`Home`}
+    description='Description will go into a meta tag in <head />'>
+    <HomepageHeader />
+    <main>
+      <HomepageFeatures />
+    </main>
+  </Layout>
+)
